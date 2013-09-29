7bb041ad3b1d014cc3c4608992d2ae2a6dd2c88a
nguyenti <tn.xc.lax@gmail.com>
2013-09-29 17:26:28 -0500

    start

---
+++ src/edu/grinnell/csc207/nguyenti/ushahidi/UshahidiExtentions.java
@@ -0,0 +1,12 @@
+package edu.grinnell.csc207.nguyenti.ushahidi;
+
+import java.io.PrintWriter;
+
+import edu.grinnell.glimmer.ushahidi.UshahidiIncident;
+
+public class UshahidiExtentions {
+
+    public static void printIncident(PrintWriter pen, UshahidiIncident incident) {
+
+    }
+}

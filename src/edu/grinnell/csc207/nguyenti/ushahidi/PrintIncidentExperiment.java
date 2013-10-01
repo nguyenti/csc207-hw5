fbbace28233d30558a5daeee4e12cdab77c5b144
Daniel <Daniel@132.161.244.78>
2013-09-30 21:17:36 -0500

    Did d and e but need to fix a couple things and comment stuff

--- src/edu/grinnell/csc207/nguyenti/ushahidi/PrintIncidentExperiment.java
+++ src/edu/grinnell/csc207/nguyenti/ushahidi/PrintIncidentExperiment.java
@@ -11,32 +11,40 @@ import edu.grinnell.glimmer.ushahidi.UshahidiWebClient;
 
 public class PrintIncidentExperiment {
 
-    public static void main(String[] args) throws Exception {
-	PrintWriter pen = new PrintWriter(System.out, true);
-	Calendar date = Calendar.getInstance();
-	date.set(2050, 12, 28);
+	public static void main(String[] args) throws Exception {
+		PrintWriter pen = new PrintWriter(System.out, true);
+		Calendar date = Calendar.getInstance();
+		date.set(2050, 12, 28);
+		Calendar earlyDate = Calendar.getInstance();
+		earlyDate.set(2024, 1, 1);
 
-	// A few basic incidents
-	UshahidiExtensions.printIncident(pen, UshahidiUtils.SAMPLE_INCIDENT);
-	UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
-	UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
+		// A few basic incidents
+		UshahidiExtensions.printIncident(pen, UshahidiUtils.SAMPLE_INCIDENT);
+		UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
+		UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
 
-	// A newly created incident
-	UshahidiIncident myIncident = new UshahidiIncident(42, "Life",
-		date, new UshahidiLocation(112358, "Galaxy"), "For Hitchhikers");
-	UshahidiExtensions.printIncident(pen, myIncident);
+		// A newly created incident
+		UshahidiIncident myIncident = new UshahidiIncident(42, "Life",
+				date, new UshahidiLocation(112358, "Galaxy"), "For Hitchhikers");
+		UshahidiExtensions.printIncident(pen, myIncident);
 
-	// One from a list
-	UshahidiClient client = UshahidiUtils.SAMPLE_CLIENT;
-	UshahidiExtensions.printIncident(pen, client.nextIncident());
+		// One from a list
+		UshahidiClient client = UshahidiUtils.SAMPLE_CLIENT;
+		UshahidiExtensions.printIncident(pen, client.nextIncident());
 
-	// One that requires connecting to the server
-	UshahidiClient webclient = new UshahidiWebClient("https://farmersmarket.crowdmap.com/");
-	UshahidiExtensions.printIncident(pen, webclient.nextIncident());
-	
-	UshahidiExtensions.lowHighId(UshahidiExtensions.ushahidiIncidenttester());
-	
-	
-    } // main(String[])
-    
+		// One that requires connecting to the server
+		UshahidiClient webclient = new UshahidiWebClient("https://farmersmarket.crowdmap.com/");
+		//UshahidiExtensions.printIncident(pen, webclient.nextIncident());
+
+//		UshahidiExtensions.lowHighId(UshahidiExtensions.ushahidiIncidenttester());
+//		UshahidiExtensions.lowHighId(webclient);
+		
+		UshahidiExtensions.printWithin(webclient, earlyDate, date);
+		//UshahidiIncident[] incidents = UshahidiExtensions.arrayWithin(webclient, earlyDate, date);
+
+//		for (int i = 0; i < incidents.length; i++) {
+//			UshahidiExtensions.printIncident(pen, incidents[i]);
+//		}
+	} // main(String[])
+
 }
--- src/edu/grinnell/csc207/nguyenti/ushahidi/UshahidiExtensions.java
+++ src/edu/grinnell/csc207/nguyenti/ushahidi/UshahidiExtensions.java
@@ -1,8 +1,8 @@
 package edu.grinnell.csc207.nguyenti.ushahidi;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Random;
 
 import edu.grinnell.glimmer.ushahidi.UshahidiClient;
 import edu.grinnell.glimmer.ushahidi.UshahidiIncident;
@@ -11,50 +11,77 @@ import edu.grinnell.glimmer.ushahidi.UshahidiLocation;
 
 public class UshahidiExtensions {
 
-    
-    public static void printIncident(PrintWriter pen, UshahidiIncident incident) {
-	pen.println("Incident #: " + incident.getId());
-	pen.println("  Title: " + incident.getTitle());
-	pen.println("  Description: " + incident.getDescription());
-	pen.println("  Date: " + (incident.getDate().get(Calendar.MONTH) + 1) + "/"
-		+ incident.getDate().get(Calendar.DATE) + "/" + incident.getDate().get(Calendar.YEAR));
-	pen.println("  Location: " + incident.getLocation());
-	pen.println("  Status: (" + incident.getMode() + ", "
-		+ incident.getActive() + ", " + incident.getVerified() + ")");
-    }
-
-    public static UshahidiIncidentList ushahidiIncidenttester() {
-	UshahidiIncidentList incidents = new UshahidiIncidentList();
-	Random rand = new Random();
-	for (int i = 0; i < 12; i++) {
-	    Calendar date = Calendar.getInstance();
-	    date.set(rand.nextInt(3000), rand.nextInt(12) + 1,
-		    rand.nextInt(28) + 1);
-	    incidents.addIncident(new UshahidiIncident(i, "Clever title " + i,
-		    date, new UshahidiLocation(Math.abs(rand.nextInt()), "Location " + i,
-			    rand.nextInt(179) + rand.nextDouble(), rand
-				    .nextInt(179) + rand.nextDouble()),
-		    "Mmm, cheese."));
+	public static void printIncident(PrintWriter pen, UshahidiIncident incident) {
+		pen.println("Incident #: " + incident.getId());
+		pen.println("  Title: " + incident.getTitle());
+		pen.println("  Description: " + incident.getDescription());
+		pen.println("  Date: " + (incident.getDate().get(Calendar.MONTH) + 1)
+				+ "/" + incident.getDate().get(Calendar.DATE) + "/"
+				+ incident.getDate().get(Calendar.YEAR));
+		pen.println("  Location: " + incident.getLocation());
+		pen.println("  Status: (" + incident.getMode() + ", "
+				+ incident.getActive() + ", " + incident.getVerified() + ")");
 	}
-	return incidents;
-    }
 
-    public static void lowHighId(UshahidiClient client) throws Exception {
-	UshahidiIncident incident;
-	UshahidiIncident high = client.nextIncident();
-	UshahidiIncident low = high;
-	while (client.hasMoreIncidents()) {
-	    incident = client.nextIncident();
-	    int id = incident.getId();
-	    if (id > high.getId()) {
-		high = incident;
-	    } else if (id < low.getId()) {
-		low = incident;
-	    }
+	public static UshahidiIncidentList ushahidiIncidenttester() {
+		UshahidiIncidentList incidents = new UshahidiIncidentList();
+		for (int i = 0; i < 12; i++) {
+			Calendar date = Calendar.getInstance();
+			date.set(3000 - (150 * i), i + 1,
+					28 - (2 * i));
+			incidents.addIncident(new UshahidiIncident(i, "Clever title " + i,
+					date, new UshahidiLocation(i,
+							"Location " + i, i * 10.5, 180 - (3 * i)), "Mmm, cheese."));
+		}
+		return incidents;
 	}
-	PrintWriter pen = new PrintWriter(System.out, true);
-	printIncident(pen, high);
-	printIncident(pen, low);
-    }
+
+	public static void lowHighId(UshahidiClient client) throws Exception {
+		UshahidiIncident incident;
+		UshahidiIncident high = client.nextIncident();
+		UshahidiIncident low = high;
+		while (client.hasMoreIncidents()) {
+			incident = client.nextIncident();
+			int id = incident.getId();
+			if (id > high.getId()) {
+				high = incident;
+			} else if (id < low.getId()) {
+				low = incident;
+			}
+		}
+		PrintWriter pen = new PrintWriter(System.out, true);
+		printIncident(pen, high);
+		printIncident(pen, low);
+	}
+
+	public static void printWithin(UshahidiClient client, Calendar start,
+			Calendar end) throws Exception {
+		PrintWriter pen = new PrintWriter(System.out, true);
+		while (client.hasMoreIncidents()) {
+			UshahidiIncident incident = client.nextIncident();
+			if (incident.getDate().compareTo(start) >= 0
+					&& incident.getDate().compareTo(end) <= 0) {
+				printIncident(pen, incident);
+			}// if
+		}// while
+	}// printWithin
+
+	public static UshahidiIncident[] arrayWithin(UshahidiClient client,
+			Calendar start, Calendar end) throws Exception {
+		UshahidiIncident[] allIncidents = client.getIncidents();
+		ArrayList<UshahidiIncident> incidentsWithin = new ArrayList<UshahidiIncident>();
+		for (int i = 0; i < allIncidents.length; i++) {
+			if (allIncidents[i].getDate().compareTo(start) >= 0
+					&& allIncidents[i].getDate().compareTo(end) <= 0) {
+				incidentsWithin.add(allIncidents[i]);
+			}// if
+		}// for
+		
+		UshahidiIncident[] stuff = new UshahidiIncident[incidentsWithin.size()];
+		for (int i = 0; i < stuff.length; i++) {
+			stuff[i] = incidentsWithin.get(i);
+		}
+		return stuff;
+	}
 
 }

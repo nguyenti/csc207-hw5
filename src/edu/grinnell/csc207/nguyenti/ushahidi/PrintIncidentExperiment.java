c46609216ea15a4ad53d11a6a93af7ba4e2dc77b
goldstei1 <dpgold94@gmail.com>
2013-10-01 12:22:55 -0500

    this is annoying

--- src/edu/grinnell/csc207/nguyenti/ushahidi/PrintIncidentExperiment.java
+++ src/edu/grinnell/csc207/nguyenti/ushahidi/PrintIncidentExperiment.java
@@ -13,18 +13,6 @@ public class PrintIncidentExperiment {
 
     public static void main(String[] args) throws Exception {
 	PrintWriter pen = new PrintWriter(System.out, true);
-	Calendar date = Calendar.getInstance();
-	date.set(2050, 12, 28);
-
-	// A few basic incidents
-	UshahidiExtensions.printIncident(pen, UshahidiUtils.SAMPLE_INCIDENT);
-	UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
-	UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
-
-	// A newly created incident
-	UshahidiIncident myIncident = new UshahidiIncident(42, "Life",
-		date, new UshahidiLocation(112358, "Galaxy"), "For Hitchhikers");
-	UshahidiExtensions.printIncident(pen, myIncident);
 	} // main(String[])
 
     
